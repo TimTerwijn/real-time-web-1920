0eb53f44df573ec900cd207454bf2c220ee60999
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-04-06 11:58:10 +0200

    routes are working

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./server/Routes.js');
 const app = express();
 
 const port = 3000;
@@ -15,39 +16,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
-
 // ROUTES!!!
-
-
-// GET first image on main page
-app.get('/', function(req, res) {
-  //get image name
-  const imageObject = require("./modules/Images.js");
-  const images = imageObject.getAllImages();
-
-  res.render('index', {"images": images, "lastId" : imageObject.getLastId(), "nextId" : imageObject.getNextId()});
-});
-
-// GET image name by id
-app.get('/image/:id', function(req, res) {
-  //get image name
-  const id = req.params.id;
-  const imageObject = require("./modules/Images.js");
-  const imageSource = imageObject.getImageNameById(id);
-
-  res.render('index', {"images": [imageSource], "lastId" : imageObject.getLastId(), "nextId" : imageObject.getNextId()});
-});
-
-// POST upload new image 
-app.get('/insert-image', function(req, res) {
-   
-  res.render('add-image', null);
-});
-
-// POST upload new image 
-app.post('/insert-image', uploadDisk.single("picture"), (req, res) => {
-  console.log(" file disk uploaded");
-  res.redirect('/');
-});
+routes.set(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
