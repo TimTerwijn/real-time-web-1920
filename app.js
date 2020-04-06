ba8e74064669b34f129d18f2497764ac19fd69be
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2020-04-06 15:02:21 +0200

    fixed a bug

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+const express = require('express')
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static('client/public'))
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/views/index.html');
